d818a878b09fdd075354d93ae6804a4c27da6fec
Paulstar200 <vitalispaul48@live.com>
2018-10-19 06:33:02 +0300

    [Feature #161334171] created my api instance and blueprint and added the resources to the api

--- app/api/V1/__init__.py
+++ app/api/V1/__init__.py
@@ -3,3 +3,41 @@ from flask_restful import Api
 
 from .views.products import PostProduct, GetAllProducts, GetEachProduct
 from .views.sales import PostSale, GetAllSales, GetEachSale
+
+productsale_api = Blueprint('resources.products', __name__, url_prefix="/api/v1")
+api = Api(productsale_api)
+
+api.add_resource(
+    PostProduct,
+    "/products",
+    endpoint="product"
+)
+
+api.add_resource(
+    GetAllProducts,
+    '/products',
+    endpoint="products"
+)
+
+api.add_resource(
+    GetEachProduct,
+    '/products/<int:id>',
+    endpoint="each")
+
+api.add_resource(
+    PostSale,
+    '/sales',
+    endpoint="sales"
+)
+
+api.add_resource(
+    GetAllSales,
+    '/sales',
+    endpoint='sale'
+)
+
+api.add_resource(
+    GetEachSale,
+    '/sales/<int:id>',
+    endpoint='eachsale'
+)
