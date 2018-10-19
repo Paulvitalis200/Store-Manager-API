783e4fc650a696dfa4543fe9155816530e8b394a
Paulstar200 <vitalispaul48@live.com>
2018-10-19 06:57:22 +0300

    [Chore #161204916] Wrote tests for post_product, get_all_products, get_each_product git commit -m [Chore

--- app/tests/V1/test_products.py
+++ app/tests/V1/test_products.py
@@ -12,3 +12,29 @@ class BaseTest(unittest.TestCase):
             "name": "James",
             "price": 2000
         }
+
+    def test_post_product(self):
+        """TEST whether the API can create a product(POST)"""
+        res = self.client.post("/api/v1/products", content_type='application/json', data=json.dumps(self.products))
+        data = json.loads(res.get_data().decode("UTF-8"))
+        self.assertEqual(res.status_code, 201)
+
+    def test_get_all_products(self):
+        res = self.client.post("api/v1/products", content_type='application/json', data=json.dumps(self.products))
+        self.assertEqual(res.status_code, 201)
+        res = self.client.get("api/v1/products")
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("James", str(res.data))
+
+    def test_get_each_product(self):
+        """Test API can get a single record by using it's id."""
+        res = self.client.post('/api/v1/products', content_type='application/json', data=json.dumps(self.products))
+        self.assertEqual(res.status_code, 201)
+        res = self.client.get("/api/v1/products/1")
+        data = json.loads(res.get_data().decode("UTF-8"))
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('James', str(res.data))
+
+
+if __name__ == "__main__":
+    unittest.main()
