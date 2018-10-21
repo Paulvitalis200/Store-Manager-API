6eccccabf07dff1bdce5ca2da0f056ba426b5d9c
Paulstar200 <vitalispaul48@live.com>
2018-10-21 17:02:11 +0300

    [Chore #161366157] Refactored the test_sales.py file

--- app/tests/V1/test_sales.py
+++ app/tests/V1/test_sales.py
@@ -1,65 +1,83 @@
-# import unittest
-# import json
+import unittest
+from flask import json
 
-# from app import create_app
+from app import create_app
 
-
-# POST_SALES_URL = '/api/v1/sales'
-# GET_EACH_SALE = '/api/v1/sales/1'
-# GET_ALL_SALES = '/api/v1/sales'
-
-
-# class BaseTest(unittest.TestCase):
-#     def setUp(self):
-#         self.app = create_app()
-#         self.client = self.app.test_client()
-#         self.sales = {"name": "Shoes", "price": 2000}
-
-#     def login(self):
-#         res = self.client.post(
-#             '/api/v1/login',
-#             data=json.dumps(dict(username="Sammy", password="1234")),
-#             content_type='application/json')
-#         return json.loads(res.get_data().decode('UTF-8'))['access_token']
-
-#     def test_post_sales(self):
-#         """TEST an API can create a sale record(POST)"""
-#         res = self.client.post(
-#             POST_SALES_URL,
-#             content_type='application/json',
-#             data=json.dumps(self.sales),
-#             headers=dict(Authorization="Bearer " + self.login())
-#         )
-#     #     data = json.loads(res.get_data().decode("UTF-8"))
-#     #     self.assertEqual(res.status_code, 201)
-
-#     # def test_get_all_sales(self):
-#     #     res = self.client.post(
-#     #         POST_SALES_URL,
-#     #         content_type='application/json',
-#     #         data=json.dumps(self.sales),
-#     #         headers=dict(Authorization="Bearer " + self.login())
-#     #     )
-#     #     self.assertEqual(res.status_code, 201)
-#     #     res = self.client.get(GET_ALL_SALES)
-#     #     self.assertEqual(res.status_code, 200)
-#     #     self.assertIn("Shoes", str(res.data))
-
-#     # def test_get_each_sale(self):
-#     #     """Test API can get a single sale record by using it's id."""
-
-#     #     res = self.client.post(
-#     #         POST_SALES_URL,
-#     #         content_type='application/json',
-#     #         data=json.dumps(self.sales),
-#     #         headers=dict(Authorization="Bearer " + self.login())
-#     #     )
-#     #     self.assertEqual(res.status_code, 201)
-#     #     res = self.client.get(GET_EACH_SALE)
-#     #     data = json.loads(res.get_data().decode("UTF-8"))
-#     #     self.assertEqual(res.status_code, 200)
-#     #     self.assertIn('Shoes', str(res.data))
+POST_SALE_URL = '/api/v1/sales'
+GET_EACH_SALE = '/api/v1/sales/1'
+GET_ALL_SALES = '/api/v1/sales'
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+class ProductTest(unittest.TestCase):
+  def setUp(self):
+    """Initialize app and define test variables"""
+    self.app = create_app('testing')
+    self.client = self.app.test_client()
+    self.sales = {
+        "description": "Piano black",
+        "items": {"id": 1, "name": "Playstation 4", "price": 20000, "quantity": 3},
+        "total": 4000
+    }
+
+    self.empty_sale_description = {
+        "description": "",
+        "items": {"id": 1, "name": "Playstation 4", "price": 20000, "quantity": 3}
+    }
+    self.empty_sale_items = {
+        "description": "Piano black",
+        "items": ""
+    }
+
+  def login(self):
+    res = self.client.post(
+        '/api/v1/login',
+        data=json.dumps(
+            dict(email="vitalispaul48@live.com", password="manu2012")
+        ),
+        content_type='application/json')
+    return json.loads(res.get_data().decode("UTF-8"))['access_token']
+
+  def test_post_sale(self):
+    """TEST whether the API can create a product(POST)"""
+    res = self.client.post(POST_SALE_URL,
+                           content_type='application/json',
+                           data=json.dumps(self.sales),
+                           headers=dict(Authorization="Bearer " + self.login())
+                           )
+    resp_data = json.loads(res.data.decode())
+    self.assertTrue(resp_data['message'] == 'Sale record created successfully!')
+    self.assertEqual(res.status_code, 201)
+
+  def test_get_all_sales(self):
+    """TEST whether the API can get all product(POST)"""
+    res = self.client.post(POST_SALE_URL,
+                           content_type='application/json',
+                           headers=dict(Authorization="Bearer " + self.login()),
+                           data=json.dumps(self.sales))
+    self.assertEqual(res.status_code, 201)
+    res = self.client.get(GET_ALL_SALES,
+                          headers=dict(Authorization="Bearer " + self.login()),
+                          content_type='application/json')
+    resp_data = json.loads(res.data.decode())
+    self.assertTrue(resp_data['message'] == 'Sales records retrieved successfully!')
+    self.assertEqual(res.status_code, 200)
+
+  # def test_get_each_product(self):
+  #   """Test API can get a single record by using it's id."""
+  #   '''Add a product'''
+  #   res = self.client.post(POST_PRODUCT_URL,
+  #                          content_type='application/json',
+  #                          headers=dict(Authorization="Bearer " + self.login()),
+  #                          data=json.dumps(self.products))
+  #   self.assertEqual(res.status_code, 201)
+  #   res = self.client.get(GET_SINGLE_PRODUCT,
+  #                         data=json.dumps(self.products),
+  #                         headers=dict(Authorization="Bearer " + self.login()),
+  #                         content_type='application/json')
+  #   data = json.loads(res.get_data().decode("UTF-8"))
+  #   self.assertEqual(res.status_code, 200)
+  #   self.assertIn('Playstation 4', str(res.data))
+
+
+if __name__ == "__main__":
+  unittest.main()
