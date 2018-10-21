f6d4f61520be2e53efdeb647b1f3d328f0be9d0f
Paulstar200 <vitalispaul48@live.com>
2018-10-21 17:15:39 +0300

    [Chore #161366157] Edited my configuration variables

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask, Blueprint
 from flask_jwt_extended import JWTManager
 
 
-def create_app():
+def create_app(config):
     app = Flask(__name__)
 
-    from instance.config import Config
-    app.config.from_object(Config)
+    from instance.config import app_config
+    app.config.from_object(app_config[config])
     app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
 
     from .api.V1 import productsale_api as psa
