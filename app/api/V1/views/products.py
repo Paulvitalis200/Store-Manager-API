2cfd107b4a0b6d817db052cbe23d38f81981f120
Paulstar200 <vitalispaul48@live.com>
2018-10-21 16:44:34 +0300

    [Chore #161366157] Refactored the products view and added basic validations

--- app/api/V1/views/products.py
+++ app/api/V1/views/products.py
@@ -1,52 +1,58 @@
-from flask import request
+from flask import Flask, jsonify, request, make_response, Blueprint
 
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from app.api.V1.models import Product
-
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 
 
 class PostProduct(Resource):
 
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', required=True, help='Product name cannot be blank', type=str)
+    parser.add_argument('price', required=True, help=' Product price cannot be blank', type=int)
+    parser.add_argument('quantity', required=True, help='Product quantity cannot be blank', type=int)
+
     @jwt_required
     def post(self):
-        data = request.get_json()
-        p = ['name', 'price']
-        for i in data.keys():
-            if i not in p:
-                return {"message": "The field {} is not needed".format(i)}, 400
-                # test
-        if 'name' not in data:
-            return {"msg": "please provide name"}, 400
-            # test
+        # input validation
+        args = PostProduct.parser.parse_args()
+        name = args.get('name').strip()  # removes whitespace
+        price = args.get('price')
+        quantity = args.get('quantity')
 
-        name = data['name']
+        if not name:
+            return make_response(jsonify({'message': 'Product name can not be empty'}), 400)
+        if not price:
+            return make_response(jsonify({'message': 'Price of product cannot be empty'}), 400)
+        if not quantity:
+            return make_response(jsonify({'message': 'Quantity of product cannot be empty'}), 400)
 
-        price = data['price']
-        # edge case and test
+        try:
+            product = Product.create_product(name, price, quantity)
+            return {
+                'message': 'Product created successfully!', 'product': product, 'status': 'ok'
+            }, 201
 
-        result = Product.create_product(name, price)
-        return {'message': 'product created succesfully',
-                'products': result,
-                'status': 'ok'
-                }, 201
+        except Exception as e:
+            print(e)
+            return {'message': 'Something went wrong.'}, 500
 
 
 class GetAllProducts(Resource):
     # Both attendant and store owner can get products
     @jwt_required
     def get(self):
-        result = Product.get_all_products()
-        return result
+        products = Product.get_products()
+        return {
+            'message': 'Products successfully retrieved!',
+            'status': 'ok',
+            'products': products
+        }, 200
 
 
+# Get a single specific product
 class GetEachProduct(Resource):
-
-    # Both attendant and store owner can get an individual product
-    @jwt_required
-    def get(self, id):
-        try:
-            result = Product.get_each_product(id)
-        except IndexError:
-            return "The Id does not exist"
-        return result
+    @staticmethod
+    def get(product_id):
+        product_index = product_id - 1
+        return products[product_index]
